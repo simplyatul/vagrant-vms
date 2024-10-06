ab8a0c4a8dd8388196b3ff7442a3cd49e1fd4104
Atul Thosar <atulthosar@gmail.com>
2024-10-06 16:51:48 +0530

    Update kind-install.sh

--- kind-install.sh
+++ kind-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.18.0/kind-linux-amd64
-chmod +x ./kind 
+# Ref: https://kind.sigs.k8s.io/docs/user/quick-start/#installing-from-release-binaries
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-arm64
+chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
