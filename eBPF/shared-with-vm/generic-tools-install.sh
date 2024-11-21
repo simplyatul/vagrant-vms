d3ec7c6a74a34f44618b9c55579d2061542dcccb
Atul Thosar <atulthosar@gmail.com>
2024-11-21 10:43:07 +0530

    update

--- eBPF/shared-with-vm/generic-tools-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Ref: https://raw.githubusercontent.com/simplyatul/vagrant-vms/main/tools-0-install.sh
-
-apt-get update
-sudo apt install -y net-tools vim curl git tree traceroute make dos2unix bind9-dnsutils
