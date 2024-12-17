f38300f32af529441de710690976099a1c69a3c8
Atul Thosar <atulthosar@gmail.com>
2024-12-17 18:47:35 +0530

    Update tools-0-install.sh

--- tools-0-install.sh
+++ tools-0-install.sh
@@ -3,4 +3,5 @@
 # Some useful packages a developer may require
 sudo apt update
 sudo apt install -y net-tools vim curl git tree traceroute make dos2unix bind9-dnsutils \
-    tshark ethtool python3 python3-pip python3-scapy iputils-ping iproute2 htop tree
+    tshark ethtool python3 python3-pip python3-scapy iputils-ping iproute2 htop tree \
+    tmux
