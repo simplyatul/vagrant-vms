a56e873eeabc1337b2da4636e797436725200722
Atul Thosar <atulthosar@gmail.com>
2025-04-19 19:11:45 +0530

    update

--- tools-0-install.sh
+++ tools-0-install.sh
@@ -4,4 +4,4 @@
 sudo apt update
 sudo apt install -y net-tools vim curl git tree traceroute make dos2unix bind9-dnsutils \
     tshark ethtool python3 python3-pip python3-scapy iputils-ping iproute2 htop tree \
-    tmux jq
+    tmux jq bridge-utils
